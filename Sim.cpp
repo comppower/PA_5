4625fc6c997d836ce5d2cb98c305988e50519a78
klibby <klibby@instructor-VirtualBox>
2017-10-09 12:54:38 -0400

    WORKING: updated arg checking

--- Sim.cpp
+++ Sim.cpp
@@ -7,6 +7,16 @@
 #include <iostream>
 #include <stdlib.h>
 #include "Board.h"
+#include <string>
+
+bool isValid(int doodle, int ants, int boardSize){
+	if(boardSize*boardSize<(doodle+ants)){
+		std::cout<<"Board to small for no. creatures specified"<<std::endl;
+		std::cout<<"Stopping Execution"<<std::endl;
+		return false;
+	}
+	return true;
+}
 
 int main(int argc, char *argv[]){
 	for(int i=1; i<argc; i++){
@@ -27,23 +37,38 @@ int main(int argc, char *argv[]){
 		srand(1);
 		break;
 	case 2:
+		if(!isValid(5, 100, atoi(argv[1]))){
+			return -1;
+		}
 		Board::Init(5,100, atoi(argv[1]), atoi(argv[1]));
 		srand(1);
 		break;
 	case 3:
+		if(!isValid(atoi(argv[2]), 100, atoi(argv[1]))){
+			return -1;
+		}
 		Board::Init(atoi(argv[2]),100, atoi(argv[1]), atoi(argv[1]));
 		srand(1);
 		break;
 	case 4:
+		if(!isValid(atoi(argv[2]), atoi(argv[3]), atoi(argv[1]))){
+			return -1;
+		}
 		Board::Init(atoi(argv[2]),atoi(argv[3]), atoi(argv[1]), atoi(argv[1]));
 		srand(1);
 		break;
 	case 5:
+		if(!isValid(atoi(argv[2]), atoi(argv[3]), atoi(argv[1]))){
+			return -1;
+		}
 		Board::Init(atoi(argv[2]),atoi(argv[3]), atoi(argv[1]), atoi(argv[1]));
 		steps=atoi(argv[4]);
 		srand(1);
 		break;
 	case 6:
+		if(!isValid(atoi(argv[2]), atoi(argv[3]), atoi(argv[1]))){
+			return -1;
+		}
 		Board::Init(atoi(argv[2]),atoi(argv[3]), atoi(argv[1]), atoi(argv[1]));
 		steps=atoi(argv[4]);
 		if(argv[5]==0){
@@ -55,6 +80,9 @@ int main(int argc, char *argv[]){
 		break;
 		//it will ignore anything 0 or below
 	case 7:
+		if(!isValid(atoi(argv[2]), atoi(argv[3]), atoi(argv[1]))){
+			return -1;
+		}
 		Board::Init(atoi(argv[2]),atoi(argv[3]), atoi(argv[1]), atoi(argv[1]));
 		steps=atoi(argv[4]);
 		if(argv[5]==0){
@@ -70,21 +98,34 @@ int main(int argc, char *argv[]){
 		return -1;
 		break;
 	}
+	//prime the pump
 	int count=0;
+	std::string temp;
+	//ouput the initial setup
+	Board::BoardPrint();
+	std::cout<<"num steps "<<count<<std::endl;
+	std::cout<<"hit any key to continue"<<std::endl;
+	std::cin>>temp;
+	//keep printing
 	for(int i=0; i<steps; i++){
 		Board::Play();
 		if(pauseVal>0){
 			if(i%pauseVal==0){
 				Board::BoardPrint();
-				count+=5;
+				count+=pauseVal;
 				std::cout<<"num steps "<<count<<std::endl;
+				std::cout<<"hit any key to continue"<<std::endl;
+				std::cin>>temp;
 			}
 		}
 	}
+	//give it that last output
 	Board::BoardPrint();
 	std::cout<<Board::GetNumAnts()<<std::endl;
 	std::cout<<Board::GetNumDoodle()<<std::endl;
+	//call it a day
 	return 0;
 }
+
 
 
